e56d9190abeaf13e0bcf430dd777f09a1e5fc6e6
diogofragoso <diogofragoso@yahoo.com.br>
2025-04-12 15:27:53 -0300

    api inserir e listar ucs ok

---
+++ pages/api/ucs/ucs.js
@@ -0,0 +1,22 @@
+import db from '../../db.js';
+import runMiddleware from '../../../middleware/cors.js';
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res);
+
+  if (req.method === 'POST') {
+    const { nome_uc, numero_uc, id_curso_fk } = req.body;
+
+    try {
+      const query = 'INSERT INTO uc (nome_uc, numero_uc, id_curso_fk) VALUES (?, ?, ?)';
+      await db.execute(query, [nome_uc, numero_uc, id_curso_fk]);
+
+      res.status(200).json({ message: 'Dados inseridos com sucesso!' });
+    } catch (error) {
+      console.error('Erro ao inserir dados:', error);
+      res.status(500).json({ error: 'Erro ao inserir dados', details: error.message });
+    }
+  } else {
+    res.status(405).json({ error: 'Método não permitido' });
+  }
+}
