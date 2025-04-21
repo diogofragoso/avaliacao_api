b9807f50aaf00a7e61a66b43398b99f0902b1e07
diogofragoso <diogofragoso@yahoo.com.br>
2025-04-21 15:06:36 -0300

    criacao dos arquivos para editar os indicadores

---
+++ pages/api/indicadores/inseririndicador.js
@@ -0,0 +1,22 @@
+import db from '../../db.js';
+import runMiddleware from '../../../middleware/cors.js';
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res);
+
+  if (req.method === 'POST') {
+    const { numero_indicador, descricao_indicador, id_uc_fk } = req.body;
+
+    try {
+      const query = 'INSERT INTO indicador (numero_indicador, descricao_indicador, id_uc_fk) VALUES (?, ?, ?)';
+      await db.execute(query, [numero_indicador, descricao_indicador, id_uc_fk]);
+
+      res.status(200).json({ message: 'Dados inseridos com sucesso!' });
+    } catch (error) {
+      console.error('Erro ao inserir dados:', error);
+      res.status(500).json({ error: 'Erro ao inserir dados', details: error.message });
+    }
+  } else {
+    res.status(405).json({ error: 'Método não permitido' });
+  }
+}
