f147d3f4e70778ad7384a460c027b348b2a3e7b5
diogofragoso <diogofragoso@yahoo.com.br>
2025-05-02 15:25:39 -0300

    inicio da criancao dos endpoints para atividades avaliativas

---
+++ pages/api/atividadeavaliativa/[id].js
@@ -0,0 +1,28 @@
+import runMiddleware from '../../../middleware/cors.js'; // Ajuste o caminho conforme necessário
+import db from '../../db.js'; // Ajuste o caminho conforme necessário
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res);
+
+  const { id } = req.query; // id será o parâmetro dinâmico na URL (ex: /api/avaliativas/1)
+
+  if (req.method === 'GET') {
+    try {
+      const rows = await db.execute(
+        'SELECT id_avaliativa, descricao_avaliativa, id_indicador_fk FROM atividade_avaliativa WHERE id_indicador_fk = ?',
+        [id] // id será o id_uc vindo da URL
+      );
+
+      if (rows.length === 0) {
+        return res.status(404).json({ message: 'Nenhuma atividade avaliativa encontrada para esta UC.' });
+      }
+
+      res.status(200).json(rows);
+    } catch (error) {
+      console.error('Erro ao listar atividades avaliativas:', error);
+      res.status(500).json({ error: 'Erro ao listar atividades avaliativas', details: error.message });
+    }
+  } else {
+    res.status(405).json({ error: 'Método não permitido' });
+  }
+}
