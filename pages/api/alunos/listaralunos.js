0ef3204bce54f940103c9bc6bfc3a6ff99ef2dc4
diogofragoso <diogofragoso@yahoo.com.br>
2025-08-16 20:49:14 -0300

    api listar alunos matriculados por turma

--- pages/api/alunos/listaralunos.js
+++ pages/api/alunos/listaralunos.js
@@ -1,13 +1,22 @@
 import db from '../../db.js';
+import runMiddleware from '../../../middleware/cors';
 
 export default async function handler(req, res) {
+  // Executa o middleware CORS
+  await runMiddleware(req, res);
+
   if (req.method === 'GET') {
     try {
-      const rows = await db.execute('SELECT id_aluno, nome_aluno, email_aluno from aluno');
+      const rows = await db.execute(
+        'SELECT id_aluno, nome_aluno, email_aluno FROM aluno'
+      );
       res.status(200).json(rows);
     } catch (error) {
-      console.error('Erro ao listar os alunos:', error.message); // Log do erro para depuração
-      res.status(500).json({ error: 'Erro ao listar os alunos', details: error.message });
+      console.error('Erro ao listar os alunos:', error.message);
+      res.status(500).json({
+        error: 'Erro ao listar os alunos',
+        details: error.message,
+      });
     }
   } else {
     res.status(405).json({ error: 'Método não permitido :-(' });
---
+++ pages/api/matricula/[id].js
@@ -0,0 +1,31 @@
+import db from '../../db.js';
+import runMiddleware from '../../../middleware/cors';
+
+export default async function handler(req, res) {
+  // Executa o middleware CORS
+  await runMiddleware(req, res);
+
+  const { id } = req.query; // id da turma
+
+  if (req.method === 'GET') {
+    try {
+      const rows = await db.execute(
+        `SELECT a.id_aluno, a.nome_aluno, a.email_aluno
+         FROM matricula m
+         JOIN aluno a ON m.id_aluno_fk = a.id_aluno
+         WHERE m.id_turma_fk = ?`,
+        [id]
+      );
+
+      res.status(200).json(rows);
+    } catch (error) {
+      console.error('Erro ao listar alunos matriculadosAQUI:', error.message);
+      res.status(500).json({
+        error: 'Erro ao listar alunos matriculados',
+        details: error.message,
+      });
+    }
+  } else {
+    res.status(405).json({ error: 'Método não permitido :-(' });
+  }
+}
